51446159f7d87824f4bfde169fc38608c35d9683
Harlliey <276095274@qq.com>
2020-12-26 23:39:00 +0800

    add readme

--- cp.c
+++ cp.c
@@ -1,4 +1,51 @@
 //
 // Created by Harrywei on 2020/12/25.
 //
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+#define BUFFERSIZE 4096
+#define COPYMODE 0644
+
+void err_handler(char*, char*);
+
+int main(int argc, char **argv)
+{
+    int fd_in, fd_out, n_chars;
+    char buf[BUFFERSIZE];
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s source destination", *argv);
+        exit(1);
+    }
+
+    if ((fd_in = open(*(argv + 1), O_RDONLY)) == -1) {
+        err_handler("Cannot open", *(argv + 1));
+    }
+
+    if ((fd_out = creat(*(argv + 2), COPYMODE)) == -1) {
+        err_handler("Cannot create", *(argv + 2));
+    }
+
+    while ((n_chars = read(fd_in, buf, BUFFERSIZE)) > 0) {
+        if (write(fd_out, buf, n_chars) != n_chars) {
+            err_handler("Write error to", *(argv + 2));
+        }
+
+        if (n_chars == -1) {
+            err_handler("Read error from", *(argv + 1));
+        }
+    }
+
+    if (close(fd_in) == -1 || close(fd_out) == -1) {
+        err_handler("Error closing files", "");
+    }
+}
+
+void err_handler(char *s1, char *s2) {
+    fprintf(stderr, "Error: %s ", s1);
+    perror(s2);
+    exit(1);
+}
---
+++ who.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <utmp.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SHOWHOST
+
+void show_info(struct utmp *record);
+void show_time(long timeval);
+
+int main()
+{
+    struct utmp current_record;
+    int utmpfd;
+    int reclen = sizeof(current_record);
+
+    if ((utmpfd = open(UTMP_FILE, O_RDONLY)) == -1) {
+        perror(UTMP_FILE);
+        exit(1);
+    }
+
+    while (read(utmpfd, &current_record, reclen) == reclen) {
+        show_info(&current_record);
+    }
+
+    close(utmpfd);
+}
+
+void show_info(struct utmp *record)
+{
+    if (record->ut_type != USER_PROCESS) return;
+
+    printf("%-8.8s", record->ut_user);
+    printf(" ");
+    printf("%-8.8s", record->ut_line);
+    printf(" ");
+    show_time(record->ut_tv.tv_sec);
+    printf(" ");
+#ifdef SHOWHOST
+    printf("(%s)", record->ut_host);
+#endif
+    printf("\n");
+}
+
+void show_time(long timeval)
+{
+    char *p = ctime(&timeval);
+    printf("%12.12s", (p + 4));
+}
